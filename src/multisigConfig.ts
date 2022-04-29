e08f8843c3dc030cc8980589501cdccfbe7c872c
Ion Vrinceanu <vrinceanuion@gmail.com>
2022-04-29 17:50:40 +0300

    added multisig contract addess & app name

--- src/multisigConfig.ts
+++ src/multisigConfig.ts
@@ -1,5 +1,6 @@
 //add a valid contract address to make it exclusive, leave empty or null for default behaviour
-export const uniqueContractAddress = '';
+export const uniqueContractAddress =
+  'erd1qqqqqqqqqqqqqpgqspkc777dql8k0cnx22n2dtpp7mkurkelrgdsv2llqq';
 
 //Optional, if you want to give a name to the visual representation of the contract
-export const uniqueContractName = 'My one and only multisig';
+export const uniqueContractName = 'Multisig SC :: wattwo';
