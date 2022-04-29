9c6f1ceda2e1abdaa87e35c9640eb5dbeabf1891
Ion Vrinceanu <vrinceanuion@gmail.com>
2022-04-29 19:15:48 +0300

    changed multisig SC address

--- src/multisigConfig.ts
+++ src/multisigConfig.ts
@@ -1,6 +1,6 @@
 //add a valid contract address to make it exclusive, leave empty or null for default behaviour
 export const uniqueContractAddress =
-  'erd1qqqqqqqqqqqqqpgqspkc777dql8k0cnx22n2dtpp7mkurkelrgdsv2llqq';
+  'erd1qqqqqqqqqqqqqpgq0rczf2dkdsap8wwwrpyqjhl8g6lvt3gjrgds7yxmc0';
 
 //Optional, if you want to give a name to the visual representation of the contract
 export const uniqueContractName = 'Multisig SC :: wattwo';
